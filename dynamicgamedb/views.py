62db962ceabe60ad3c706aed6d404ba01315aceb
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-15 21:21:39 +0100

    Small change.

--- dynamicgamedb/views.py
+++ dynamicgamedb/views.py
@@ -1,5 +1,9 @@
 from dynamicgamedb import app
-import flask
+from dynamicgamedb.database import db_session
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 @app.route('/', methods=['GET'])
 def home():
