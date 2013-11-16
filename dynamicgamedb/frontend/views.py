5eaf9e268bfdce6d9410b7bef0ceeca832e013e2
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-16 16:43:36 +0100

    Added docs api endpoint

--- dynamicgamedb/frontend/views.py
+++ dynamicgamedb/frontend/views.py
@@ -5,3 +5,7 @@ from flask import request, jsonify, redirect, url_for
 @frontend.route('/games', methods=['GET'])
 def home():
     return "Many games.. beautiful html and much css."
+
+@frontend.route('/docs/api', methods=['GET'])
+def api_doc():
+    return "Much api info"
