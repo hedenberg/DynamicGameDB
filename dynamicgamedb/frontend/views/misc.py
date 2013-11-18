0848571f364105d986d6af3b41654d52583b3e54
Andreas Hägglund <gegga87@gmail.com>
2013-11-18 15:39:50 +0100

    started adding frontend frame

--- dynamicgamedb/frontend/views/misc.py
+++ dynamicgamedb/frontend/views/misc.py
@@ -4,3 +4,18 @@ from flask import request, jsonify, redirect, url_for, render_template
 @frontend.route('/dynamicgamedb/frontend/static/css/<path:filename>')
 def send_foo(filename):
      return send_from_directory('dynamicgamedb/frontend/static/css', filename)
+
+
+
+
+@frontend.route('/')
+def index():
+    return render_template('index.html')
+
+@frontend.route('/about')
+def about():
+    return render_template('about.html')
+
+@frontend.route('/contact')
+def contact():  
+    return render_template('contact.html')
