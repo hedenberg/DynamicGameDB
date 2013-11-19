22f90568e8b5b592b541512a329cfba79c6d5dd7
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-19 16:15:53 +0100

    Played with init endpoints and fixed small bug in game endpoints

--- dynamicgamedb/backend/__init__.py
+++ dynamicgamedb/backend/__init__.py
@@ -4,6 +4,6 @@ from jinja2 import TemplateNotFound
 backend = Blueprint('backend', __name__,
                     template_folder='templates')
 
-from database import init_db
+from dynamicgamedb.backend.database import init_db
 init_db()
 import dynamicgamedb.backend.views
--- dynamicgamedb/backend/views/game.py
+++ dynamicgamedb/backend/views/game.py
@@ -6,23 +6,39 @@ from flask import request, jsonify, redirect, url_for
 
 @backend.route('/api/games', methods=['GET'])
 def games():
+    print "api games"
     games = db_session.query(Game).order_by(Game.id)
     return jsonify({"games":[{"game_id":game.id,
                               "game_title":game.title,
                               "platform":game.platform.name,
                               "developer":game.developer} for game in games]})
 
-@backend.route('/api/game/add', methods=['GET','POST'])
+@backend.route('/api/game/add', methods=['POST'])
 def add_game():
-    print "Backend add_game: ", request.form
-    platform = db_session.query(Platform).get(request.form['platform_id'])
-    game = Game(request.form['title'], platform)
-    db_session.add(game)
-    try:
+    title = request.form['title']
+    platform_id = str(request.form['platform_id'])
+    print "add"
+    games = Game.query.filter(Game.title.like(title)).all()
+    print "games: ", games
+    exists = False
+    for game in games:
+        print "game.title: ", game.title
+        print "title: ", title
+        print "game.pid: ", game.platform_id
+        print "pid: ", platform_id
+        if game.title == title and str(game.platform_id) == platform_id:
+            exists = True
+    if not exists:
+        platform = db_session.query(Platform).get(request.form['platform_id'])
+        if platform == None:
+            print "Platform doesn't exist"
+            return jsonify({"error":"Platform doesn't exist"})
+        game = Game(request.form['title'], platform)
+        db_session.add(game)
         db_session.commit()
-    except Exception, e:
-        return "Failed"
-    print "here2"
+    else:
+        print "Already existed"
+        game = games[0]
     return jsonify({"game_id":game.id,
                     "game_title":game.title,
                     "platform":game.platform.name,
