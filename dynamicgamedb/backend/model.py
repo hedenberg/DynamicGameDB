de8c0fadaaa513555a5b251559e6cc36b66fde5d
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-22 14:58:34 +0100

    ...

--- dynamicgamedb/backend/model.py
+++ dynamicgamedb/backend/model.py
@@ -5,9 +5,9 @@ from database import Base
 
 class Game(Base):
     __tablename__ = 'games'
-    id = Column(Integer, primary_key=True)
+    g_id = Column(Integer, primary_key=True)
     title = Column(String(250))
-    platform_id = Column(Integer, ForeignKey('platforms.id'))
+    platform_id = Column(Integer, ForeignKey('platforms.p_id'))
     platform = relationship("Platform", backref=backref('games'))
     picture = Column(String(250))
     info = Column(String(500), default="")
@@ -18,7 +18,7 @@ class Game(Base):
     # TODO: change so that it takes a dicitonary instead.
     def __init__(self, title=None, platform=None):
         self.title = title
-        self.platform_id = platform.id
+        self.platform_id = platform.p_id
         self.platform = platform
 
     def __repr__(self):
@@ -26,7 +26,7 @@ class Game(Base):
 
 class Platform(Base):
     __tablename__ = "platforms"
-    id = Column(Integer, primary_key=True)
+    p_id = Column(Integer, primary_key=True)
     name = Column(String(250))
 
     def __init__(self, name=None):
