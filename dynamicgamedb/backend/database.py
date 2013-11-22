f49357a64489c7dfc93256c77a357dcf838ac9cf
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-22 15:09:40 +0100

    ..

--- dynamicgamedb/backend/database.py
+++ dynamicgamedb/backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 #engine = create_engine('sqlite:///dynamicgame.db', convert_unicode=True)
-engine = create_engine(os.environ['DATABASE_URL'], convert_unicode=True)
+engine = create_engine('postgres://jmcqxvdgcsvsyl:BlvratbaAe05GQYS6HQIjX_ZIX@ec2-184-73-254-144.compute-1.amazonaws.com:5432/d10k9sruavbqno', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
