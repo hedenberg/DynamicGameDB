7adc0efc4db610627f6ba0f9fedfe1ebbbd4418a
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-27 16:14:29 +0100

    Added endpoints for relations between games

--- dynamicgamedb/backend/views/game.py
+++ dynamicgamedb/backend/views/game.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from dynamicgamedb.backend import backend
-from dynamicgamedb.backend.model import Game, Platform
+from dynamicgamedb.backend.model import Game, Platform, Relation
 from dynamicgamedb.backend.database import db_session
 from flask import request, jsonify, redirect, url_for
 
@@ -73,3 +73,55 @@ def edit_game(id):
                     "game_title":game.title,
                     "platform":game.platform.name,
                     "developer":game.developer})
+@backend.route('/api/game/<int:id>/relation', methods=['GET', 'POST'])
+def game_relation(id):
+    if request.method == 'GET':
+        # All relations including this game
+        relations = Relation.query.filter((Relation.game1_id==id)|(Relation.game2_id==id)).all()
+        rs = [(r.game1_id, r.count) if not r.game1_id == id else (r.game2_id, r.count) for r in relations]
+        # All games with either part of the relation except this game
+        #games = db_session.query(Game).filter((Game.g_id!=id)&(Game.g_id.in_(map(lambda r:r.game1_id, relations))|Game.g_id.in_(map(lambda r:r.game2_id, relations))))
+        games = db_session.query(Game).filter(Game.g_id.in_(map(lambda (r, c):r, rs)))
+        rgs = zip((map(lambda (r,c):c, rs)),games)
+        rgs = sorted(rgs,key=lambda rg: rg[0], reverse=True)
+        """
+        return jsonify({"games":[{"game_id":game.g_id,
+                        "game_title":game.title,
+                        "platform":game.platform.name,
+                        "platform_id":game.platform.p_id,
+                        "developer":game.developer} for (_,game) in rgs],
+                        "relations":[{"game_id":game.g_id,
+                        "count":count} for (count,game) in rgs]})
+        """
+        return jsonify({"relatedgames":[{"game_id":game.g_id,
+                        "game_title":game.title,
+                        "platform":game.platform.name,
+                        "platform_id":game.platform.p_id,
+                        "developer":game.developer,
+                        "count":c} for (c,game) in rgs]})
+    else:
+        if id == request.form['g_id']:
+            return jsonify({"error":"Can not relate to self"})
+        g1id = id if id < request.form['g_id'] else request.form['g_id']
+        g2id = request.form['g_id'] if id < request.form['g_id'] else id
+        g1 = db_session.query(Game).get(g1id)
+        g2 = db_session.query(Game).get(g2id)
+        if g1 == None:
+            return jsonify({"error":"Source id doesn't exist"})
+        if g2 == None:
+            return jsonify({"error":"Target id doesn't exist"})
+        #Always least id first
+        relation = db_session.query(Relation).get((g1.g_id,g2.g_id))
+        if relation == None:
+            print "Relation was None"
+            relation = Relation(g1,g2)
+            db_session.add(relation)
+        else:
+            relation.count = relation.count + 1
+        try:
+            db_session.commit()
+        except Exception, e:
+            return "Failed"
+        return jsonify({"g1_id":relation.game1_id,
+                        "g2_id":relation.game2_id,
+                        "count":relation.count})
---
+++ dynamicgamedb/backend/views/relation.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from dynamicgamedb.backend import backend
+from dynamicgamedb.backend.model import Game, Platform, Relation
+from dynamicgamedb.backend.database import db_session
+from flask import request, jsonify, redirect, url_for
+
+
+@backend.route('/api/relations', methods=['GET'])
+def relations():
+    relations = db_session.query(Relation).order_by(Relation.game1_id)
+    return jsonify({"relations":[{"g1_id":relation.game1_id,
+                                  "g2_id":relation.game2_id,
+                                  "count":relation.count} for relation in relations]})
+
+@backend.route('/api/relation/<int:id>', methods=['GET'])
+def relation(id):
+    relations = Relation.query.filter((Relation.game1_id==id)|(Relation.game2_id==id)).all()
+    return jsonify({"relations":[{"g1_id":relation.game1_id,
+                                  "g2_id":relation.game2_id,
+                                  "count":relation.count} for relation in relations]})
+
+@backend.route('/api/relation/add', methods=['POST'])
+def add_relation():
+    in1 = request.form['g1_id']
+    in2 = request.form['g2_id']
+    if in1 == in2:
+        return jsonify({"error":"Can not relate to self"})
+    g1id = in1 if in1 < in2 else in2
+    g2id = in2 if in1 < in2 else in1
+    g1 = db_session.query(Game).get(g1id)
+    g2 = db_session.query(Game).get(g2id)
+    if g1 == None:
+        return jsonify({"error":"Source id doesn't exist"})
+    if g2 == None:
+        return jsonify({"error":"Target id doesn't exist"})
+    #Always least id first
+    relation = db_session.query(Relation).get((g1.g_id,g2.g_id))
+    if relation == None:
+        print "Relation was None"
+        relation = Relation(g1,g2)
+        db_session.add(relation)
+    else:
+        relation.count = relation.count + 1
+    try:
+        db_session.commit()
+    except Exception, e:
+        return "Failed"
+    return jsonify({"g1_id":relation.game1_id,
+                    "g2_id":relation.game2_id,
+                    "count":relation.count})
