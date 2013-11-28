a003cc784ad6ac660d7a6454327c4baebf7b4f07
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-28 16:18:38 +0100

    ..

--- dynamicgamedb/frontend/views/docs.py
+++ dynamicgamedb/frontend/views/docs.py
@@ -4,3 +4,7 @@ from flask import request, jsonify, redirect, url_for, render_template, send_fro
 @frontend.route('/docs/api', methods=['GET'])
 def api_doc():
     return render_template('api_doc.html') 
+
+@frontend.route('/docs/api_v2', methods=['GET'])
+def api():
+    return render_template('api.html') 
