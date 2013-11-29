d69c3ecd9073bd4f5d60418493b06f29e0635042
Linus Hedenberg <linushedenberg@gmail.com>
2013-11-29 13:56:01 +0100

    Standard image for new games

--- dynamicgamedb/backend/model.py
+++ dynamicgamedb/backend/model.py
@@ -9,7 +9,7 @@ class Game(Base):
     title = Column(String(250))
     platform_id = Column(Integer, ForeignKey('platforms.p_id'))
     platform = relationship("Platform", backref=backref('games'))
-    picture = Column(String(250))
+    picture = Column(String(250), default="http://i.imgur.com/g4zDXeF.png")
     info = Column(String(500), default="")
     release_date = Column(DateTime, default=datetime.now)
     developer = Column(String(250), default="")
