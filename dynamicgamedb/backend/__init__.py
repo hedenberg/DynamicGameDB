48940cd0fc168dbbcf17f3cf7d5b4ea764229665
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-07 15:34:23 +0100

    Added oid here where it will be accessed

--- dynamicgamedb/backend/__init__.py
+++ dynamicgamedb/backend/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from flask.ext.openid import OpenID
+
+oid = OpenID( '../openid')
 
 backend = Blueprint('backend', __name__,
                     template_folder='templates')
