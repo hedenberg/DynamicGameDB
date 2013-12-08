6207421be53b83da1867387271e2b55a2cfc6f61
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-08 15:22:14 +0100

    Added request that includes token in header.

--- dynamicgamedb/frontend/api_com/__init__.py
+++ dynamicgamedb/frontend/api_com/__init__.py
@@ -2,6 +2,7 @@ import urllib
 import httplib2
 import json
 
+from flask import session
 from dynamicgamedb.frontend.api_com.models import User, Game, Platform, Relation, GameRelation
 
 #API_URL = "http://dynamicgamedb.herokuapp.com"
@@ -18,7 +19,7 @@ class DynamicGameDB(object):
 
     def games(self,search_term):
         games_dict = dict(search_term=search_term)
-        response, content = self.request("/games",  
+        response, content = self.request(endpoint="/games",  
                                          method="POST",
                                          body=games_dict)
         if not response.status == 200:
@@ -28,7 +29,7 @@ class DynamicGameDB(object):
         return [Game.from_dict(game) for game in json.loads(content).get("games")]
 
     def get_games(self):
-        response, content = self.request("/games")
+        response, content = self.request(endpoint="/games")
 
         if not response.status == 200:
             pass
@@ -37,7 +38,7 @@ class DynamicGameDB(object):
 
 
     def game(self, id):
-        response, content = self.request("/game/%d"%id)
+        response, content = self.request(endpoint="/game/%d"%id)
         if not response.status == 200:
             print "/game/id/ error"
             pass
@@ -45,9 +46,9 @@ class DynamicGameDB(object):
 
     def add_game(self, title, platform_id):
         game_dict = dict(title=title, platform_id=str(platform_id))
-        response, content = self.request("/game/add", 
-                                         method="POST", 
-                                         body=game_dict)
+        response, content = self.login_required_request(endpoint="/game/add", 
+                                                        method="POST", 
+                                                        body=game_dict)
         if not response.status == 200:
             print "/game/add/ error"
             pass
@@ -62,7 +63,7 @@ class DynamicGameDB(object):
                          release_date=release_date, 
                          developer=developer,
                          publisher=publisher)
-        response, content = self.request("/game/%d/edit"%id, 
+        response, content = self.request(endpoint="/game/%d/edit"%id, 
                                          method="POST",
                                          body=game_dict)
         if not response.status == 200:
@@ -73,14 +74,14 @@ class DynamicGameDB(object):
     # -- Platform --
 
     def platforms(self):
-        response, content = self.request("/platforms")
+        response, content = self.request(endpoint="/platforms")
         if not response.status == 200:
             pass
             #Should probably handle error
         return [Platform.from_dict(platform) for platform in json.loads(content).get("platforms")]
 
     def platform(self, id):
-        response, content = self.request("/platform/%d"%id)
+        response, content = self.request(endpoint="/platform/%d"%id)
         if not response.status == 200:
             print "/game/id/ error"
             pass
@@ -88,7 +89,7 @@ class DynamicGameDB(object):
 
     def add_platform(self, name):
         platform_dict = dict(name=title)
-        response, content = self.request("/platform/add", method="POST", body=platform_dict)
+        response, content = self.request(endpoint="/platform/add", method="POST", body=platform_dict)
         if not response.status == 200:
             print "/game/add/ error"
             pass
@@ -99,7 +100,7 @@ class DynamicGameDB(object):
     
     def game_relations(self,id):
         print "game relation api_com"
-        response, content = self.request("/game/%d/relation"%id)
+        response, content = self.request(endpoint="/game/%d/relation"%id)
         if not response.status == 200:
             print "/game/id/relation GET error"
             pass
@@ -107,7 +108,7 @@ class DynamicGameDB(object):
         return [GameRelation.from_dict(game_relation) for game_relation in json.loads(content).get("relatedgames")]
 
     def game_relation(self,s_id,t_id):     #s_id = sourceId t_id = targetId
-        response,content = self.request("/game/%d/relation/%d"%(s_id,t_id))
+        response,content = self.request(endpoint="/game/%d/relation/%d"%(s_id,t_id))
         if not response.status == 200:
             print "/game/sId/relation/tId GET error"
             pass
@@ -115,7 +116,7 @@ class DynamicGameDB(object):
 
     def add_game_relation(self,s_id,t_id):
         relation_dict = dict(g_id=t_id)
-        response, content = self.request("/game/%d/relation"%s_id, method="POST", body=relation_dict)
+        response, content = self.request(endpoint="/game/%d/relation"%s_id, method="POST", body=relation_dict)
         if not response.status == 200:
             print "/game/id/relation POST error"
             pass
@@ -128,22 +129,41 @@ class DynamicGameDB(object):
 
     def auth_token(self, one_time_token):
         auth_dict = dict(client_id=str(self.client_id), client_secret=self.client_secret, one_time_token=str(one_time_token))
-        response, content = self.request("/token/", method="POST", body=auth_dict)
+        response, content = self.request(endpoint="/token/", method="POST", body=auth_dict)
         if not response.status == 200:
             print "/token/ POST error"
             pass
         self.token = content
+        self.session_user()
         return content
 
-    def user(self):
+    def session_user(self):
+        print "Frontend session user"
         auth_dict = dict(client_id=str(self.client_id), client_secret=self.client_secret, token=str(self.token))
-        response, content = self.request("/user/", method="POST", body=auth_dict)
+        response, content = self.request(endpoint="/user/", method="POST", body=auth_dict)
         if not response.status == 200:
             print "/user/ POST error"
             pass
-        return User.from_dict(json.loads(content))
+        user = User.from_dict(json.loads(content))
+        session["user_email"] = user.email
+
+    def user(self):
+        print "Frontend user get thingy"
+        auth_dict = dict(client_id=str(self.client_id), client_secret=self.client_secret, token=str(self.token))
+        response, content = self.request(endpoint="/user/", method="POST", body=auth_dict)
+        if not response.status == 200:
+            print "/user/ POST error"
+            pass
+        user = User.from_dict(json.loads(content))
+        #session["user_email"] = user.email
+        return user
 
     # -- General --
+
+    def login_required_request(self, endpoint, method="GET", headers=dict(), body=None):
+        headers.update(
+            token=self.token)
+        return self.request(endpoint, method, headers, body)
 
     def request(self, endpoint, method="GET", headers=dict(), body=None):
         http = httplib2.Http()
@@ -155,3 +175,15 @@ class DynamicGameDB(object):
                                          headers=headers,
                                          body=body)
         return response, content
+
+    def error_handler(self, content):
+        error_dict = json.loads(content).get("error")
+        raise exceptions.DBDBError(
+            type=error_dict.get("type"),
+            message=error_dict.get("message"))
+
+
+class DGDBError(Exception):
+    def __init__(self, type, message):
+        Exception.__init__(self, message)
+        self.type = type
