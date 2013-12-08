67a93eeb36018880661302c4e6aea604d9695d7e
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-08 15:17:27 +0100

    Added before request handeler that sets the bakcned_client and backend_user variables. The actual check happens in a later stage.

--- dynamicgamedb/backend/__init__.py
+++ dynamicgamedb/backend/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, g, request
 from jinja2 import TemplateNotFound
 from flask.ext.openid import OpenID
 from functools import wraps
@@ -6,7 +6,7 @@ from functools import wraps
 oid = OpenID( '../openid')
 
 class BackendBlueprint(Blueprint):
-    def require_client(self, f):
+    def client_required(self, f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             if not g.api_client:
@@ -14,10 +14,10 @@ class BackendBlueprint(Blueprint):
             return f(*args, **kwargs)
         return decorated_function
 
-    def require_user(self, f):
+    def user_required(self, f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if not g.user:
+            if not g.backend_user:
                 return """{"error":"Not logged in"}"""
             return f(*args, **kwargs)
         return decorated_function
@@ -28,4 +28,26 @@ backend = BackendBlueprint('backend', __name__,
 
 from dynamicgamedb.backend.database import init_db
 init_db()
+from dynamicgamedb.backend.database import db_session
 import dynamicgamedb.backend.views
+from dynamicgamedb.backend.model import User, Client
+
+@backend.before_request
+def user_client_auth():
+    g.backend_client = None
+    g.backend_user = None
+
+    client_id = request.headers.get('client_id', None) or request.args.get('client_id', None)
+    client_secret = request.headers.get('client_secret', None) or request.args.get('client_secret', None)
+
+    if client_id and client_secret:
+        client = db_session.query(Client).get(client_id)
+        if client:
+            if client.c_secret == client_secret:
+                g.backend_client = client
+
+    token = request.headers.get('token', None) or request.args.get('token', None)
+    if token:
+        user = db_session.query(User).filter(User.token.like(token)).first()
+        if user:
+            g.backend_user = user
