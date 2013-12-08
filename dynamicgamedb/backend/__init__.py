e4c0ffafbf70c643aa97d79cc61289b2ad0a19ad
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-08 13:18:44 +0100

    Added require_client and require_user to blueprint class so that we can require client and users on endpoints

--- dynamicgamedb/backend/__init__.py
+++ dynamicgamedb/backend/__init__.py
@@ -1,10 +1,29 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 from flask.ext.openid import OpenID
+from functools import wraps
 
 oid = OpenID( '../openid')
 
-backend = Blueprint('backend', __name__,
+class BackendBlueprint(Blueprint):
+    def require_client(self, f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.api_client:
+                return """{"error":"No valid client"}"""
+            return f(*args, **kwargs)
+        return decorated_function
+
+    def require_user(self, f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.user:
+                return """{"error":"Not logged in"}"""
+            return f(*args, **kwargs)
+        return decorated_function
+
+
+backend = BackendBlueprint('backend', __name__,
                     template_folder='templates')
 
 from dynamicgamedb.backend.database import init_db
