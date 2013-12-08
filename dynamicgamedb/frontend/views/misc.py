fc0c225d33ad5128737685daddafb392fa99e93d
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-08 13:31:10 +0100

    Removed user-related endpoints from misc

--- dynamicgamedb/frontend/views/misc.py
+++ dynamicgamedb/frontend/views/misc.py
@@ -5,25 +5,6 @@ from dynamicgamedb.frontend import frontend, dgdb
 import sys, traceback
 from flask import request, jsonify, redirect, url_for, render_template, send_from_directory, session
 
-#@frontend.route('/dynamicgamedb/frontend/static/<path:filename>')
-#def send_foo(filename):
-    #print "filename", filename
-#    return send_from_directory('frontend/static', filename)
-
-
-@frontend.route('/login/')
-def new_login():
-    return redirect(dgdb.api_login_url())
-
-@frontend.route('/auth/')
-def auth_onetime():
-    print "frontend auth"
-    if request.args.get("one_time_token"):
-        print "One time token: ", request.args.get("one_time_token")
-        token = dgdb.auth_token(request.args.get("one_time_token"))
-        session["user_token"] = token
-    print "redirect to /"
-    return redirect("/")
 
 
 @frontend.route('/')
@@ -32,20 +13,7 @@ def index():
     games = dgdb.get_games()
     #search = "GET"
     return render_template("games.html", games=games, search="GET")
-"""
-def index():
-    print "INDEX"
-    try:
-        return render_template('index.html')
-    except:
-        print "Exception in user code:"
-        print '-'*60
-        traceback.print_exc(file=sys.stdout)
-        print '-'*60
-    """
 
-    
-    
 
 @frontend.route('/about')
 def about():
@@ -54,13 +22,3 @@ def about():
 @frontend.route('/contact')
 def contact():  
     return render_template('contact.html')
-
-
-
-        # try:
-        #     return render_template('index.html')
-        # except:
-        #     print "Exception in user code:"
-        #     print '-'*60
-        #     traceback.print_exc(file=sys.stdout)
-        #     print '-'*60
