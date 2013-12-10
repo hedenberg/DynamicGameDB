31c1ec2075230c3419a8820b24bbd26165022176
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-10 09:12:32 +0100

    Heroku values

--- dynamicgamedb/backend/views/user.py
+++ dynamicgamedb/backend/views/user.py
@@ -6,8 +6,8 @@ from dynamicgamedb.backend.database import db_session
 from flask import Response, request, jsonify, redirect, url_for, session, g
 import dateutil.parser
 
-FRONTEND_URL = 'http://localhost:8000'
-#FRONTEND_URL = 'http://dynamicgamedb.herokuapp.com'
+#FRONTEND_URL = 'http://localhost:8000'
+FRONTEND_URL = 'http://dynamicgamedb.herokuapp.com'
 
 @backend.route('/api/logout/')
 def logout():
